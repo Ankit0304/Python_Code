8d536d3681dc2e5d67e36dc2363817c3a3f7786f
Ankit Baingane <115683256+Ankit0304@users.noreply.github.com>
2025-01-10 20:07:59 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,35 +1,41 @@
 
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+while True:
+    
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-choice = input("Enter choice (1/2/3/4): ")
+    print(''' Select Operation: 
+1.Addition
+2.Subtraction
+3.Multiplication
+4.Division''')
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+    choice = input("Enter choice (1/2/3/4): ")
 
-if choice == '1':
-    result = num1 + num2
-    operation = "+"
-elif choice == '2':
-    result = num1 - num2
-    operation = "-"
-elif choice == '3':
-    result = num1 * num2
-    operation = "*"
-elif choice == '4':
-    if num2 != 0:
-        result = num1 / num2
+    if choice == '1':
+        result = num1 + num2
+        operation = "+"
+    elif choice == '2':
+        result = num1 - num2
+        operation = "-"
+    elif choice == '3':
+        result = num1 * num2
+        operation = "*"
+    elif choice == '4':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error! Division by zero."
+            operation = "/"
     else:
-        result = "Error! Division by zero."
-    operation = "/"
-else:
-    result = "Invalid Input"
-    operation = ""
+        result = "Invalid Input"
+        operation = ""
 
-if operation:
-    print(num1, operation, num2, "=", result)
-else:
-    print(result)
+    if operation:
+        print(num1, operation, num2, "=", result)
+    else:
+        print(result)
+        
+     next_calculation = input("Do you want to perform another calculation? (y/n): ")
+    if next_calculation.lower() != "y":
+        break
